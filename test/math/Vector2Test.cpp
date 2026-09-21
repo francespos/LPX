a1e2655bcf5842e61f4a54750f364535f84a323e
francespos <f.esposito.email@gmail.com>
2026-09-21 21:44:04 +0200

    .

--- test/math/Vector2Test.cpp
+++ test/math/Vector2Test.cpp
@@ -46,10 +46,10 @@ TEST(Vector2Test, AddOperator) {
     Vector2 lhs(x1, y1);
     Vector2 rhs(x2, y2);
 
-    Vector2 expected_sum = Vector2(x1 + x2, y1 + y2);
-    Vector2 actual_sum = lhs + rhs;
+    Vector2 expected = Vector2(x1 + x2, y1 + y2);
+    Vector2 actual = lhs + rhs;
 
-    EXPECT_EQ(expected_sum, actual_sum);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, SubtractOperator) {
@@ -62,10 +62,10 @@ TEST(Vector2Test, SubtractOperator) {
     Vector2 lhs(x1, y1);
     Vector2 rhs(x2, y2);
 
-    Vector2 expected_difference = Vector2(x1 - x2, y1 - y2);
-    Vector2 actual_difference = lhs - rhs;
+    Vector2 expected = Vector2(x1 - x2, y1 - y2);
+    Vector2 actual = lhs - rhs;
 
-    EXPECT_EQ(expected_difference, actual_difference);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, MultiplyVectorScalar) {
@@ -73,10 +73,10 @@ TEST(Vector2Test, MultiplyVectorScalar) {
     float y = -432.02f; 
     float scalar = -6532.04f;
 
-    Vector2 expexted_product(x * scalar, y * scalar);
-    auto actual_product = Vector2(x, y) * scalar;
+    Vector2 expexted(x * scalar, y * scalar);
+    auto actual = Vector2(x, y) * scalar;
 
-    EXPECT_EQ(expexted_product, actual_product);
+    EXPECT_EQ(expexted, actual);
 }
 
 TEST(Vector2Test, MultiplyScalarVector) {
@@ -84,10 +84,10 @@ TEST(Vector2Test, MultiplyScalarVector) {
     float y = -432.02f; 
     float scalar = -6532.04f;
 
-    Vector2 expexted_product(x * scalar, y * scalar);
-    auto actual_product = scalar * Vector2(x, y);
+    Vector2 expexted(x * scalar, y * scalar);
+    auto actual = scalar * Vector2(x, y);
 
-    EXPECT_EQ(expexted_product, actual_product);
+    EXPECT_EQ(expexted, actual);
 }
 
 TEST(Vector2Test, DivideOperator) {
@@ -95,10 +95,10 @@ TEST(Vector2Test, DivideOperator) {
     float y = -432.02f; 
     float scalar = -6532.04f;
 
-    Vector2 expexted_quotient(x / scalar, y / scalar);
-    auto actual_quotient = Vector2(x, y) / scalar;
+    Vector2 expexted(x / scalar, y / scalar);
+    auto actual = Vector2(x, y) / scalar;
 
-    EXPECT_EQ(expexted_quotient, actual_quotient);
+    EXPECT_EQ(expexted, actual);
 }
 
 TEST(Vector2Test, Dot) {
@@ -108,10 +108,10 @@ TEST(Vector2Test, Dot) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
 
-    auto expected_dot = x1 * x2 + y1 * y2;
-    auto actual_dot = Dot(Vector2(x1, y1), Vector2(x2, y2));
+    auto expected = x1 * x2 + y1 * y2;
+    auto actual = Dot(Vector2(x1, y1), Vector2(x2, y2));
 
-    EXPECT_EQ(expected_dot, actual_dot);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Cross) {
@@ -121,10 +121,10 @@ TEST(Vector2Test, Cross) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
     
-    auto expected_cross = x1 * y2 - y1 * x2;
-    auto actual_cross = Cross(Vector2(x1, y1), Vector2(x2, y2));
+    auto expected = x1 * y2 - y1 * x2;
+    auto actual = Cross(Vector2(x1, y1), Vector2(x2, y2));
 
-    EXPECT_EQ(expected_cross, actual_cross);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Distance) {
@@ -134,10 +134,10 @@ TEST(Vector2Test, Distance) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
     
-    auto expected_distance = std::sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-    auto actual_distance = Distance(Vector2(x1, y1), Vector2(x2, y2));
+    auto expected = std::sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    auto actual = Distance(Vector2(x1, y1), Vector2(x2, y2));
 
-    EXPECT_EQ(expected_distance, actual_distance);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, DistanceSquared) {
@@ -147,10 +147,10 @@ TEST(Vector2Test, DistanceSquared) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
     
-    auto expected_distance_squared = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-    auto actual_distance_squared = DistanceSquared(Vector2(x1, y1), Vector2(x2, y2));
+    auto expected = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    auto actual = DistanceSquared(Vector2(x1, y1), Vector2(x2, y2));
 
-    EXPECT_EQ(expected_distance_squared, actual_distance_squared);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, AddEqualOperator) {
@@ -160,13 +160,13 @@ TEST(Vector2Test, AddEqualOperator) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
 
-    Vector2 lhs(x1, y1);
-    Vector2 rhs(x2, y2);
+    Vector2 other(x2, y2);       
+    Vector2 expected(x1 + x2, y1 + y2);
 
-    lhs += rhs;
-    
-    EXPECT_EQ(lhs.x, x1 + x2);
-    EXPECT_EQ(lhs.y, y1 + y2);
+    Vector2 actual(x1, y1);
+    actual += other;
+
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, SubtractEqualOperator) {
@@ -176,76 +176,73 @@ TEST(Vector2Test, SubtractEqualOperator) {
     float x2 = 483.04f;
     float y2 = 5002.85f;
 
-    Vector2 lhs(x1, y1);
-    Vector2 rhs(x2, y2);
+    Vector2 other(x2, y2);
+    Vector2 expected(x1 - x2, y1 - y2);
 
-    lhs -= rhs;
-    
-    EXPECT_EQ(lhs.x, x1 - x2);
-    EXPECT_EQ(lhs.y, y1 - y2);    
+    Vector2 actual(x1, y1);
+    actual -= other;
+
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, MultiplyEqualOperator) {
     float x = 3534.34f;
     float y = -53.00584f;
 
-    Vector2 vector(x, y);
     float scalar = 43.5f;
+    Vector2 expected(x * scalar, y * scalar);
 
-    vector.x *= scalar;
-    vector.y *= scalar;
+    Vector2 actual(x, y);
+    actual *= scalar;
 
-    EXPECT_EQ(vector.x, x * scalar);
-    EXPECT_EQ(vector.y, y * scalar);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, DivideEqualOperator) {
     float x = 3534.34f;
     float y = -53.00584f;
 
-    Vector2 vector(x, y);
     float scalar = 43.5f;
+    Vector2 expected(x / scalar, y / scalar);
 
-    vector.x /= scalar;
-    vector.y /= scalar;
+    Vector2 actual(x, y);
+    actual /= scalar;
 
-    EXPECT_EQ(vector.x, x / scalar);
-    EXPECT_EQ(vector.y, y / scalar);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Length) {
     float x = 394.43f;
     float y = 5003.593f;
 
-    auto expected_length = std::sqrt(x * x + y * y);
+    auto expected = std::sqrt(x * x + y * y);
 
     Vector2 vector(x, y);
-    auto actual_length = vector.Length();
+    auto actual = vector.Length();
 
-    EXPECT_EQ(expected_length, actual_length);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, LengthSquared) {
     float x = 394.43f;
     float y = 5003.593f;
 
-    auto expected_length_squared = x * x + y * y;
+    auto expected = x * x + y * y;
 
     Vector2 vector(x, y);
-    auto actual_length_squared = vector.LengthSquared();
+    auto actual = vector.LengthSquared();
 
-    EXPECT_EQ(expected_length_squared, actual_length_squared);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Negative) {
     float x = 394.43f;
     float y = 5003.593f;
 
-    Vector2 expected_negative(-x, -y);
-    auto actual_negative = Vector2(x, y).Negative();
+    Vector2 expected(-x, -y);
+    auto actual = Vector2(x, y).Negative();
 
-    EXPECT_EQ(expected_negative.x, actual_negative.x);
-    EXPECT_EQ(expected_negative.y, actual_negative.y);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Normalized) {
@@ -254,22 +251,22 @@ TEST(Vector2Test, Normalized) {
 
     auto inv_len = 1.0f / Vector2(x, y).Length();
 
-    auto expected_normalized = Vector2(x * inv_len, y * inv_len);
-    auto actual_normalized = Vector2(x, y).Normalized();
+    auto expected = Vector2(x * inv_len, y * inv_len);
+    auto actual = Vector2(x, y).Normalized();
 
-    EXPECT_EQ(expected_normalized, actual_normalized);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Negate) {
     float x = 394.43f;
     float y = 5003.593f;
 
-    auto expected_negative = Vector2(-x, -y);
+    auto expected = Vector2(-x, -y);
 
-    Vector2 actual_negative(x, y);
-    actual_negative.Negate();
+    Vector2 actual(x, y);
+    actual.Negate();
 
-    EXPECT_EQ(expected_negative, actual_negative);
+    EXPECT_EQ(expected, actual);
 }
 
 TEST(Vector2Test, Normalize) {
@@ -277,11 +274,11 @@ TEST(Vector2Test, Normalize) {
     float y = 5003.593f;
 
     auto inv_len = 1.0f / Vector2(x, y).Length();
-    Vector2 expected_normalized(x * inv_len, y * inv_len);
+    Vector2 expected(x * inv_len, y * inv_len);
 
-    Vector2 actual_normalized(x, y);
-    actual_normalized.Normalize();
+    Vector2 actual(x, y);
+    actual.Normalize();
 
-    EXPECT_EQ(expected_normalized, actual_normalized);
+    EXPECT_EQ(expected, actual);
 }
 
